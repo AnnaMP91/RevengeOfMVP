b9ac3ceca0e3fe042a3474ae5cbd442d505876af
Anna <annamp727@gmail.com>
2021-12-10 19:26:13 -0600

    Finished BMR functionality

--- client/src/components/DogResult.jsx
+++ client/src/components/DogResult.jsx
@@ -7,12 +7,20 @@ class DogResult extends React.Component {
   }
 
   render() {
-    //console.log(this.props);
     return (
       <div>
-        <img src={this.props.dogResult[0].photos[0].medium}></img>
-        <p>{this.props.dogResult[0].name}</p>
-        <a className="btn btn-primary" href={this.props.dogResult[0].url} target="_blank">Adopt me uwu</a>
+        {this.props.dogResult.length > 0 ?
+          <>
+            <img src={this.props.dogResult[0].photos[0].medium}></img>
+            <p>{this.props.dogResult[0].name}</p>
+            <a className="btn btn-primary" href={this.props.dogResult[0].url} target="_blank">Adopt me uwu</a>
+          </> :
+          <div>
+            <p>No matches in your area</p>
+            <Button href="/">Take me back</Button>
+          </div>
+        }
+
       </div>
     )
   }
